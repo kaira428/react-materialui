bb94b29f00bba492c16bcf3b45ef2853460473d5
Wong Keng <kaira428@gmail.com>
2023-03-18 21:24:44 +0800

    Add createTheme and  ThemeProvider

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -5,59 +5,89 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
 import { AccessTime } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+            color: "blue",
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://image.cnbcfm.com/api/v1/image/107090171-1658155987037-slack-imgs.jpg?v=1658156107&w=630&h=354&ffmt=webp&vtcrop=y"
-          alt="Plane"
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Airplanes
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <AccessTime sx={{ width: 12.5 }} />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            5 hours
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-          marginTop={3}
-        >
-          <Rating
-            name="read-only"
-            value={4.5}
-            readOnly
-            precision={0.5}
-            size="small"
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://image.cnbcfm.com/api/v1/image/107090171-1658155987037-slack-imgs.jpg?v=1658156107&w=630&h=354&ffmt=webp&vtcrop=y"
+            alt="Plane"
+            className="img"
           />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            4.5
-          </Typography>
-          <Typography variant="body2" component="p" marginLeft={1.5}>
-            (777 reviews)
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6" component="h3" marginTop={0}>
-            From SGD 1,234
-          </Typography>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Airplanes
+            </Typography>
           </Box>
-      </Paper>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 12.5, marginLeft: 0.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+              sx={{ marginLeft: 0.5 }}
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              (777 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginTop={0} marginLeft={1}>
+              From SGD 1,234
+            </Typography>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
