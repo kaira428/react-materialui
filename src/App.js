828e5204d952bded5039c18730bb79385eea45e4
Wong Keng <kaira428@gmail.com>
2023-03-18 21:37:27 +0800

    Add AppBar and data.json

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import SearchAppBar from "./components/AppBar"
 
 function App() {
   return (
-    <Container>
-      <Grid container spacing={5}>
-        <TourCard />
-        <TourCard />
-        <TourCard />
-        <TourCard />
-      </Grid>
-      
-    </Container>
-  )
+    <>
+      <SearchAppBar />
+      <Container sx={{marginTop: 5}}>
+        <Grid container spacing={5}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
+    </>
+  );
 }
 
 export default App;
